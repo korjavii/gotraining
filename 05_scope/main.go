6265a8794d0254795a60dbb5b0324714aedc818e
Korjavii <korjavii.dyu@gmail.com>
2017-04-02 21:12:55 +0300

    scope update

--- 05_scope/main.go
+++ 05_scope/main.go
@@ -11,8 +11,18 @@ func main() {
 
 	z := 43
 	fmt.Println(z)
+	{
+		zz := 33
+		fmt.Println(zz)
+	}
+	fmt.Println(increment())
 }
 
 func foo() {
 	fmt.Println(y)
+}
+
+func increment() int {
+	x++
+	return x
 }
